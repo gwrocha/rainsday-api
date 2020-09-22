0f94a6c932b11bd4646194f6766920c0d148b850
Gleydson Rocha <gleydson@gwrocha.com>
2020-09-21 21:25:25 -0300

    Commit inicial

---
+++ src/main/java/com/gwrocha/rainsday/RainsdayApplication.java
@@ -0,0 +1,13 @@
+package com.gwrocha.rainsday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainsdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainsdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwrocha/rainsday/RainsdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwrocha.rainsday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainsdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
